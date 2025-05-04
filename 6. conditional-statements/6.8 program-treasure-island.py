0ebaea303caf51c978422f179ba33fcbfec118c4
naikjithin <naikjithin@gmail.com>
2025-05-04 23:02:12 +0530

    Update 6.8 program-treasure-island.py

--- 6. conditional-statements/6.8 program-treasure-island.py
+++ 6. conditional-statements/6.8 program-treasure-island.py
@@ -2,7 +2,8 @@
 
 # Your goal today is to build a "Chose your own adventure game". Using what you have learnt in the lessons today you will be building a very simple version of this type of text game.
 #
-# Use the flow chart linked here to create the game logic. https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload#%7B%22pageId%22%3A%22C5RBs43oDa-KdzZeNtuy%22%7D
+# Use the flow chart linked here to create the game logic. 
+<a href="https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload#%7B%22pageId%22%3A%22C5RBs43oDa-KdzZeNtuy%22%7D">Flowchart here</a>
 #
 # Once you've completed the project, you can always extend the game and make it more interesting!
 
