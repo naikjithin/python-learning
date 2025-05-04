97e9e5fc057211a073030fa63cd79cc038e91fff
Jithin <naikjithin@gmail.com>
2025-05-04 22:00:40 +0530

    feat: data types

---
+++ 4. data types/4.3 data-types.py
@@ -0,0 +1,20 @@
+# Subscripting
+
+# string
+name = "Hello"
+print(name[0]) # h
+print(name[len(name) -1 ]) # o
+print(name[-1])  # o
+
+# integer
+print( 1 + 2 + 4)
+
+# large int
+print(123_456_789)
+
+# float
+print(3.1418)
+
+# boolean
+print(True)
+print(False)
