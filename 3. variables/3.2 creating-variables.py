920655e544b62e54f87d5ee32cd2920d8ddd9e9c
Jithin <naikjithin@gmail.com>
2025-05-04 19:08:12 +0530

    feat: variables

---
+++ 3. variables/3.2 creating-variables.py
@@ -0,0 +1,13 @@
+# variables - getting user input and printing length of string
+
+print(len(input("Enter the name to check the length? ")))
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+# output
+# Enter the name to check the length? Test
+# 4
+# What is your name? Test
+# 4
---
+++ 3. variables/3.3 band-name-generator.py
@@ -0,0 +1,17 @@
+# Band Name Generator
+# 1. Create a greeting for your program.
+# 2. Ask the user for the city that they grew up in and store it in a variable.
+# 3. Ask the user for the name of a pet and store it in a variable.
+# 4. Combine the name of their city and pet and show them their band name.
+
+print("Welcome to the band name generator project \n")
+city = input("Enter the city that you grew up in \n")
+name = input("Enter the name of a pet \n")
+print("Your band name could be: " + city + " | " + name)
+
+# output
+# Enter the city that you grew up in
+# Calicut
+# Enter the name of a pet
+# Gypsy
+# Your band name could be: Calicut | Gypsy
